698adaac9fda88cb807f2a0de1a0061530578f96
sparsh264 <68681245+sparsh264@users.noreply.github.com>
2020-07-23 13:21:09 +0530

    Add files via upload

---
+++ 3_live_face_det (1).py
@@ -0,0 +1,36 @@
+import cv2
+import sys
+
+cascPath ='C:/Users/DEll/Desktop/haarcascades 4/haarcascades_frontalface_alt.xml'
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+     
+
+    faces = faceCascade.detectMultiScale()
+    gray,
+    scaleFactor=1.15,
+    minNeighbors=5
+    
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+#C:\Users\DEll\Desktop\haarcascades 4\haarcascades
